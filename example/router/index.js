67904712f8880415edfdde07ce5a9fcaf4c99a50
panrui <panrui@mujoy.com>
2021-06-07 10:45:47 +0800

    refactor:修复代码格式

--- example/router/index.js
+++ example/router/index.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: panrui
  * @Date: 2021-05-21 18:02:50
- * @LastEditTime: 2021-06-04 18:10:15
+ * @LastEditTime: 2021-06-07 10:44:59
  * @LastEditors: panrui
  * 不忘初心,不负梦想
  */
@@ -16,45 +16,45 @@ const routes = [{
     path: "/",
     name: "Home",
     component: Home,
-    // redirect: '/start',
+    // redirect: "/start",
     children: [{
-        path: 'start',
-        name: 'Start',
+        path: "start",
+        name: "Start",
         component: () => import( /* webpackChunkName: "about" */ "../views/Start.vue"),
       },
       {
-        path: 'country',
-        name: 'Country',
+        path: "country",
+        name: "Country",
         component: () => import( /* webpackChunkName: "about" */ "../views/Country.vue"),
       },
       {
-        path: 'select',
-        name: 'Select',
+        path: "select",
+        name: "Select",
         component: () => import( /* webpackChunkName: "about" */ "../views/Select.vue"),
       },
       {
-        path: 'selectYear',
-        name: 'SelectYear',
+        path: "selectYear",
+        name: "SelectYear",
         component: () => import( /* webpackChunkName: "about" */ "../views/SelectYear.vue"),
       },
       {
-        path: 'cityPicker',
-        name: 'CityPicker',
+        path: "cityPicker",
+        name: "CityPicker",
         component: () => import( /* webpackChunkName: "about" */ "../views/CityPicker.vue"),
       },
       {
-        path: 'wangeditor',
-        name: 'Wangeditor',
+        path: "wangeditor",
+        name: "Wangeditor",
         component: () => import( /* webpackChunkName: "about" */ "../views/Wangeditor.vue"),
       },
       {
-        path: 'dateSelect',
-        name: 'DateSelect',
+        path: "dateSelect",
+        name: "DateSelect",
         component: () => import( /* webpackChunkName: "about" */ "../views/DateSelect.vue"),
       },
       {
-        path: 'upload',
-        name: 'Upload',
+        path: "upload",
+        name: "Upload",
         component: () => import( /* webpackChunkName: "about" */ "../views/Upload.vue"),
       }
     ]
--- packages/index.js
+++ packages/index.js
@@ -1,42 +1,48 @@
 /*
- * @Description: 
+ * @Description: 引入组件
  * @Author: panrui
  * @Date: 2021-05-21 18:23:19
- * @LastEditTime: 2021-06-04 18:17:30
+ * @LastEditTime: 2021-06-07 10:44:12
  * @LastEditors: panrui
  * 不忘初心,不负梦想
  */
-import MjCountry from './country'
-import MjSelect from './select'
-import MjSelectYear from './selectyear'
-import MjCityPicker from './citypicker'
-import MjWangeditor from './wangeditor'
-import MjDateSelect from './dateselect'
-import MjUpload from './upload'
+import MjCountry from "./country";
+import MjSelect from "./select";
+import MjSelectYear from "./selectyear";
+import MjCityPicker from "./citypicker";
+import MjWangeditor from "./wangeditor";
+import MjDateSelect from "./dateselect";
+import MjUpload from "./upload";
 
 // 所有组件列表
 const components = [
-  MjCountry, MjSelect, MjSelectYear, MjCityPicker, MjWangeditor, MjDateSelect, MjUpload
-]
+  MjCountry,
+  MjSelect,
+  MjSelectYear,
+  MjCityPicker,
+  MjWangeditor,
+  MjDateSelect,
+  MjUpload,
+];
 
 // 定义 install 方法，接收 Vue 作为参数
 const install = function (Vue) {
   // 判断是否安装，安装过就不继续往下执行
-  if (install.installed) return
-  install.installed = true
+  if (install.installed) return;
+  install.installed = true;
   // 遍历注册所有组件
-  components.map(component => Vue.component(component.name, component))
+  components.map((component) => Vue.component(component.name, component));
   // 下面这个写法也可以
   // components.map(component => Vue.use(component))
-}
+};
 
 // 检测到 Vue 才执行，毕竟我们是基于 Vue 的
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
 }
 
 export default {
   install,
   // 所有组件，必须具有 install，才能使用 Vue.use()
-  ...components
-}
+  ...components,
+};
